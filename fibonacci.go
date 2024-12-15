f01cde6140204c009c458e1d84ac932a66703dd7
Yara-ib <yr.ap.23@gmail.com>
2024-12-15 20:48:30 +0200

    Fibonacci Fn | Comments

--- fibonacci.go
+++ fibonacci.go
@@ -18,11 +18,13 @@ func fibo(num int) ([]int, int) {
 	numBefore = 1
 	numBeforeTheBefore = 0
 	for i := 2; i <= num; i++ {
+		// F(n) = F(n-1) + F(n-2)
 		curr = numBefore + numBeforeTheBefore
 		list = append(list, curr)
+		// F(n-2) = F(n-1) >> Moving Forward to next n
 		numBeforeTheBefore = numBefore
+		// F(n-1) = F(n) >> Moving Forward to next n
 		numBefore = curr
-
 	}
 	return list, list[len(list)-1]
 }
